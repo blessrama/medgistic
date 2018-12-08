5a4e00375b111e7327fed5381e41afe26dafc760
Dedy <dedyjoomla@gmail.com>
2018-12-08 23:50:00 +0700

    yes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,9 +19,13 @@
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
     <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/landing-page.min.css" rel="stylesheet">
+    <link href="css/login_signup.css" rel="stylesheet">
+
+
 
   </head>
   <body>
@@ -38,19 +44,18 @@
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+              <a class="nav-link" href="#about">About</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Products</a>
+              <a class="nav-link" href="#products">Products</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Partners</a>
+              <a class="nav-link" href="#partners">Partners</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">News</a>
-              </li>
-            <li class="nav-item">
-              <a class="btn btn-primary" href="#">Sign In</a>
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#signInModal">
+                Sign In
+              </button>
             </li>
           </ul>
         </div>
@@ -65,36 +70,36 @@
           <div class="col-xl-9 mx-auto">
             <h1 class="mb-5">Solusi untuk keperluan logistik medis rumah sakit anda</h1>
           </div>
-          <div class="col-md-4 mx-auto">
-            <button type="purchase" class="btn btn-block btn-lg btn-primary">Beli Sekarang!</button>
+          <div class="col-md-6 mx-auto" >
+            <!-- <button class="btn btn-block btn-lg btn-primary" href="product/product.html">Beli Sekarang!</button> -->
+            <a class="btn btn-primary btn-block btn-lg" href="#products" role="button">Beli Sekarang!</a>
           </div>
         </div>
       </div>
     </header>
 
     <!-- Image Showcases -->
-    <section class="showcase">
+    <section class="showcase" id="about">
       <div class="container-fluid p-0">
         <div class="row no-gutters">
-
           <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
           <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-            <h2>Fully Responsive Design</h2>
-            <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+            <h2>Harga Terjangkau</h2>
+            <p class="lead mb-0">Lebih hemat dan terjangkau, tiada biaya tersembunyi dan jelas pembiayaannya daripada harus membuat sistem logistik sendiri</p>
           </div>
         </div>
         <div class="row no-gutters">
           <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
           <div class="col-lg-6 my-auto showcase-text">
-            <h2>Updated For Bootstrap 4</h2>
-            <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
+            <h2>Efisien</h2>
+            <p class="lead mb-0">Dibuat sesuai kebutuhan client kami, mulai dari klinik pribadi hingga Rumah Sakit Internasional</p>
           </div>
         </div>
         <div class="row no-gutters">
           <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
           <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-            <h2>Easy to Use &amp; Customize</h2>
-            <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+            <h2>Dukungan Teknisi 24/7</h2>
+            <p class="lead mb-0">Teknisi dan layanan pelanggan kami siap melayani anda untuk segala problema yang akan terjadi!</p>
           </div>
         </div>
       </div>
@@ -102,7 +107,7 @@
 
   <section class="features-icons bg-light text-center">
     <div class="container">
-      <h2 class="mb-5">Tawaran produk dari kami</h2>
+      <h2 class="mb-5" id="products">Tawaran produk dari kami</h2>
       <div class="row">
         <div class="row">
 
@@ -111,7 +116,7 @@
               <a href="#"><img class="card-img-top" src="img/puskesmas.jpg" alt=""></a>
               <div class="card-body">
                 <h4 class="card-title">
-                  <a href="#">Paket Fasilitas Kesehatan 1</a>
+                  <a href="product/product.html" role="button">Paket Fasilitas Kesehatan 1</a>
                 </h4>
                 <h5>$3,499.00</h5>
                 <p class="card-text">Sangat menguntungkan bagi penggunaan pada fasilitas kesehatan tingkat 1: Puskesmas, Poliklinik</p>
@@ -124,10 +129,10 @@
 
           <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="img/pexels-photo-263399.jpeg" alt=""></a>
+              <a href="#ProductModal2"><img class="card-img-top" src="img/pexels-photo-263399.jpeg" alt=""></a>
               <div class="card-body">
                 <h4 class="card-title">
-                  <a href="#">Paket Fasilitas Kesehatan 2</a>
+                  <a href="product/product.html" role="button">Paket Fasilitas Kesehatan 2</a>
                 </h4>
                 <h5>$5,499.00</h5>
                 <p class="card-text">Sangat menguntungkan bagi penggunaan pada fasilitas kesehatan tingkat 2: Rumah Sakit tipe B dan C</p>
@@ -143,7 +148,7 @@
               <a href="#"><img class="card-img-top" src="img/pexels-photo-668300.jpeg" alt=""></a>
               <div class="card-body">
                 <h4 class="card-title">
-                  <a href="#">Paket Fasilitas Kesehatan 3</a>
+                  <a href="product/product.html" role="button">Paket Fasilitas Kesehatan 3</a>
                 </h4>
                 <h5>$8,499.00</h5>
                 <p class="card-text">Sangat menguntungkan bagi penggunaan pada fasilitas kesehatan tingkat 3: Rumah Sakit tipe A</p>
@@ -193,7 +198,7 @@
     <!-- Icons Grid -->
     <section class="features-icons bg-light text-center">
       <div class="container">
-        <h2 class="mb-5">Partner Kami</h2>
+        <h2 class="mb-5" id="partners">Partner Kami</h2>
         <div class="row">
           <div class="col-lg-4">
             <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
@@ -290,6 +295,118 @@
         </div>
       </div>
     </footer>
+
+    <!-- Sign In Modal -->
+    <div class="modal fade" id="signInModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="ProductModal3">Masuk</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body col-lg-12 login-form-1">
+                <h3>Selamat datang kembali!</h3>
+                <form>
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Alamat Email *" value="" />
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password Anda *" value="" />
+                  </div>
+                  <div class="form-group">
+                    <input type="submit" class="btnSubmit" value="Login" />
+                  </div>
+                  <div class="form-group">
+                    <a data-dismiss="modal" data-toggle="modal" href="#signUpModal" class="ForgetPwd">Belum punya akun? Daftar dulu!</a>
+                  </div>
+                </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+      <!-- Sign Up Modal -->
+      <div class="modal fade" id="signUpModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="ProductModal3">Daftar</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body col-lg-12 login-form-1">
+                    <h3>Halo Pendatang! Silahkan Daftar Dulu Yaaaa!</h3>
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
