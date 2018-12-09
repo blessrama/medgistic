cff6d33edf85675aa45616d24e15fb3b77990612
Dedy <dedyjoomla@gmail.com>
2018-12-09 21:59:54 +0700

    Update 1.blade.php

--- resources/views/product/1.blade.php
+++ resources/views/product/1.blade.php
@@ -53,7 +53,7 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light static-top">
       <div class="container">
-        <a class="navbar-brand" href="/">Medgistic</a>
+        <a class="navbar-brand" href="{{url('/')}}">Medgistic</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
